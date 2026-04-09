f69082ea1ae7561767898f9ad0f5a973c6b0fc5d
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2026-04-09 20:45:24 +0400

    Book management blueprint

---
+++ FinalProjectsStep1/BM.ConsoleUI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BM.ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
