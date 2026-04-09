058c0689592d0fe9924ca9a675fb8f6d597bcb7c
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2026-04-09 20:42:17 +0400

    Student management blueprint

---
+++ FinalProjectsStep1/SM.ConsoleUI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SM.ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
